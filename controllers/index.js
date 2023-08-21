30f815e21ba1d4e128975d865080ae99d1efc51f
kaysenjwilson <kwilson26@s.tooeletech.edu>
2023-08-21 12:13:39 -0600

    first commit

---
+++ controllers/index.js
@@ -0,0 +1,10 @@
+const awesomeFunction = (req, res) => {
+    res.send("Hello World!");
+};
+
+const thisHard = (req, res) => {
+    res.send("Tooele Tech is awesome");
+};
+
+
+module.exports = { awesomeFunction, thisHard };
--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
+app.use("/", require("./routes"));
 
 app.listen(3000, () => {
     console.log(`Test server running on port: ${port}`);
